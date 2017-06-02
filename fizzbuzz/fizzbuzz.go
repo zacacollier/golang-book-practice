f06fb6e6aee6a9c71b58f309f7c5c4a6f0205614
Zac Collier <zacacollier@gmail.com>
2017-06-02 09:55:23 -0500

    Refactor fizzbuzz

--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -6,15 +6,14 @@ import (
 
 func main() {
   for i := 1; i <= 100; i++ {
-    if i % 3 == 0 {
-      if i % 5 == 0 {
-        fmt.Println("FizzBuzz")
-      } else {
-        fmt.Println("Fizz")
-      }
-    } else if i % 5 == 0 {
-      fmt.Println("Buzz")
-    } else {
+    switch i % 3 {
+    case 0:
+        if i % 5 == 0 {
+          fmt.Println("FizzBuzz")
+        } else {
+          fmt.Println("Fizz")
+        }
+    default:
       fmt.Println(i)
     }
   }
