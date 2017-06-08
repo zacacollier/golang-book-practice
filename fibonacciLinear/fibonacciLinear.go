f48c074ad3d4d0269cf941785fcac2738aee7ec8
Zac Collier <zacacollier@gmail.com>
2017-06-08 14:03:19 -0500

    Add linear fibonacci solution

---
+++ fibonacciLinear/fibonacciLinear.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func fib(n int) int {
+  var n2, n1, next int
+  n2 = 1
+  n1 = 0
+  for n > 0 {
+    next = n2
+    n2 = n2 + n1
+    n1 = next
+    fmt.Println(n2)
+    n--
+  }
+  return n2
+}
+
+func main() {
+  fmt.Println(fib(10))
+}
