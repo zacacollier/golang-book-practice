d0340fccc4d9db97fa68e534701bf75d4fdf2ec3
Zac Collier <zacacollier@gmail.com>
2017-06-12 15:36:14 -0500

    Add binarySearch successful attempt

--- binarySearch/binarySearch.go
+++ binarySearch/binarySearch.go
@@ -11,25 +11,30 @@ func Round(val float64) int {
 
 func binarySearch(seq []int, i int) int {
   seq = seq[:]
-  mid := Round(float64(len(seq))) / 2
-  last := len(seq) - 1
-  fmt.Println(mid, last, seq[0:mid], seq[mid:])
-  switch {
-  case seq[0] == i:
-    return 0
-  case seq[mid] == i:
-    return mid
-  case seq[last] == i:
-    return last
-  case seq[mid] < i:
-    return binarySearch(seq[mid:], i)
-  case seq[mid] > i:
-    return binarySearch(seq[0:mid], i)
-  default:
+  low := 0
+  high := len(seq) - 1
+  var mid int
+ //  if (i > seq[high]) {
+ //    return -1
+ //  }
+  for low <= high {
+    mid = low + Round(float64(high - low)) / 2
+    if (seq[mid] < i) {
+      low = mid + 1
+      fmt.Println("low:", low, "mid:", mid, "high:", high)
+      fmt.Println("seq:", seq)
+    } else {
+      high = mid - 1
+    }
+  }
+  if (low == len(seq) || seq[low] != i) {
     return -1
+  } else {
+    return low
   }
 }
 
 func main() {
   fmt.Println(binarySearch([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 7))
+  fmt.Println(binarySearch([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 11))
 }
